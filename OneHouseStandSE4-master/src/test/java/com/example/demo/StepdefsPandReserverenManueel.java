942d24b9a0c7e38eb1eb59b66c8de658a2dedd31
Pierre Vanvolsem <pierre@vanvolsem.com>
2019-11-23 00:29:25 +0100

    scenario's opdracht 3 deel 3

---
+++ OneHouseStandSE4-master/src/test/java/com/example/demo/StepdefsPandReserverenManueel.java
@@ -0,0 +1,102 @@
+package com.example.demo;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.Before;
+import cucumber.api.java.nl.Als;
+import cucumber.api.java.nl.Dan;
+import cucumber.api.java.nl.En;
+import cucumber.api.java.nl.Gegeven;
+import org.junit.After;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class StepdefsPandReserverenManueel {
+    private WebDriver driver;
+
+    @Before
+    public void setUp() {
+        //todo clean data
+
+        String pathToGeckoDriver = "C:\\Users\\Pierre\\Downloads\\geckodriver-v0.26.0-win64\\geckodriver.exe";
+        //Setting webdriver.gecko.driver property
+        System.setProperty("webdriver.gecko.driver", pathToGeckoDriver );
+        //Instantiating driver object and launching browser
+        driver = new FirefoxDriver();
+    }
+
+    @After
+    public void end(){
+        driver.quit();
+    }
+
+    @Gegeven("^er is een bestaande pand met id (\\d+) die goedgekeurd is en die geen reservering heeft$")
+    public void erIsEenBestaandePandMetIdDieGoedgekeurdIsEnDieGeenReserveringHeeft(int arg0) {
+        //run script
+    }
+
+    @En("^er is een huurder Tibo ingelogd\\.$")
+    public void erIsEenHuurderTiboIngelogd() {
+        driver.navigate().to("http://localhost:8080/");
+
+        driver.findElement(By.name("logout")).click();
+
+        new WebDriverWait(driver, 10).until(ExpectedConditions
+                .textToBePresentInElementLocated(By.tagName("body"), "Je bent uitgelogd"));
+        driver.navigate().to("http://localhost:8080/login");
+
+        new WebDriverWait(driver, 10).until(ExpectedConditions
+                .textToBePresentInElementLocated(By.tagName("body"), "login page"));
+
+        driver.findElement(By.id("username")).sendKeys("tibo");
+        driver.findElement(By.id("password")).sendKeys("tibo");
+        driver.findElement(By.name("submit")).click();
+
+        new WebDriverWait(driver, 2); //pagina kan varieren, dus kan niet dynamisch
+    }
+
+    @En("^Tibo is op de pagina van de pand met id (\\d+)$")
+    public void tiboIsOpDePaginaVanDePandMetId(int id) {
+        driver.navigate().to("http://localhost:8080/pand?id="+id);
+    }
+
+    @Als("^Tibo het veld \"([^\"]*)\" invuld met (\\d+)$")
+    public void tiboHetVeldInvuldMet(String elementname, int inhoud) throws Throwable {
+        driver.findElement(By.name(elementname)).sendKeys(Integer.toString(inhoud));
+    }
+
+    @En("^Tibo het veld \"([^\"]*)\" invuld  met \"([^\"]*)\"$")
+    public void tiboHetVeldInvuldMet(String elementname, String inhoud) throws Throwable {
+        if (elementname == "einddatum"){
+            inhoud = inhoud.replace("/","");
+        }
+        driver.findElement(By.id(elementname)).sendKeys(inhoud);
+    }
+
+    @Dan("^staat er een nieuwe lijn onder Bestaande reservaties met \"([^\"]*)\"$")
+    public void staatErEenNieuweLijnOnderBestaandeReservatiesMet(String reservatie) throws Throwable {
+        String bodytext = driver.findElement(By.tagName("body")).getText();
+        Assert.assertTrue(bodytext.contains(reservatie));
+    }
+
+    @Dan("^staat er op de error pagina \"([^\"]*)\"$")
+    public void staatErOpDeErrorPagina(String errorbericht) throws Throwable {
+        String bodytext = driver.findElement(By.tagName("body")).getText();
+        Assert.assertTrue(bodytext.contains(errorbericht));
+    }
+
+    @Gegeven("^de pand met id (\\d+) heeft één reservatie met datum \"([^\"]*)\" en aantal personen (\\d+)$")
+    public void dePandMetIdHeeftÉénReservatieMetDatumEnAantalPersonen(int arg0, String arg1, int arg2) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+        //todo script
+    }
+
+    @En("^Tibo op reserveer Klikt$")
+    public void tiboOpReserveerKlikt() {
+        driver.findElement(By.name("submit")).click();
+    }
+}
